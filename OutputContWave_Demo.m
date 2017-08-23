61aa4d288ee8dc373eb5af14ded693aecc22ef62
oiwic <fortune@mail.ustc.edu.cn>
2017-08-23 20:39:39 +0800

    添加测试文件
    OutputContWave用来控制DAC输出连续波形

---
+++ OutputContWave_Demo.m
@@ -0,0 +1,15 @@
+da = USTCDAC('10.0.2.4',80);
+da.Open();
+da.Init();
+da.SetTrigSel(0);
+da.StartStop(15);
+waveobj = waveform();
+data = waveobj.generate_squr();
+seq = waveobj.generate_seq(length(data));
+num = 0;
+for k = 1:4
+    da.WriteWave(k,0,data);
+    da.WriteSeq(k,0,seq);
+end
+da.CheckStatus();
+da.Close();
